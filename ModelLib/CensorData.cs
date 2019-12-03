820b194eefc754f31842d829a3122f1a72edad99
lassegh <lasse.groenbech@gmail.com>
2019-12-03 10:57:28 +0100

    Initial commit

---
+++ ModelLib/CensorData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelLib
+{
+    public class CensorData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Temperature { get; set; }
+        public int Co2 { get; set; }
+    }
+}
---
+++ SencorRest/Controllers/CensorsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib;
+
+namespace SencorRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CensorsController : ControllerBase
+    {
+        private static List<CensorData> censorsList = new List<CensorData>();
+
+        private int nextId = 0;
+
+        // GET: api/Censors
+        [HttpGet]
+        public IEnumerable<CensorData> Get()
+        {
+            return censorsList;
+        }
+
+        // GET: api/Censors/5
+        [HttpGet("{id}")]
+        public CensorData Get(int id)
+        {
+            return censorsList.FirstOrDefault(c => c.Id == id);
+        }
+
+        // POST: api/Censors
+        [HttpPost]
+        public void Post([FromBody] CensorData value)
+        {
+            value.Id = nextId;
+            censorsList.Add(value);
+            nextId++;
+        }
+    }
+}
